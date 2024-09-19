672d1b1280346c134e676a56154e912412b61038
baozitong <zitongbao1215@gmail.com>
2024-09-19 08:42:04 -0700

    Add in landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import Labs from './Labs';
 import Kanbas from './Kanbas';
-import { HashRouter, Route, Routes} from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 export default function App() {
   return (
     <HashRouter>
       <div>
-        <Link to="/Labs">Labs</Link> | <Link to="/Kanbas">Kanbas</Link>
+        <h1>Zitong Bao - Online Section</h1>
+        <h4>My GitHub repo</h4>
+        Please 
+        <a
+          id='wd-lipsum'
+          href='https://github.com/bzt0143/kanbas-react-web-app'
+        >
+          click here
+        </a>
+        to my github repo
+        <br />
+        <br />
+        <Link to='/Labs'>Labs</Link> | <Link to='/Kanbas'>Kanbas</Link>
         <Routes>
           <Route path='/Labs/*' element={<Labs />} />
           <Route path='/Kanbas/*' element={<Kanbas />} />
